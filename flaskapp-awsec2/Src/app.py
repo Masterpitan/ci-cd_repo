45a821769c32d7bad49ecba3db7198bd27a41f21
Masterpitan <akoredeadetunji93@gmail.com>
2025-05-23 17:49:09 +0100

    Pushing new changes

--- flaskapp-awsec2/Src/app.py
+++ flaskapp-awsec2/Src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1> This is Adepitan Adetunji</h1><br><p>Thank you for checking out my page. Stay tuned for my DevOps journey.</p> '
+    return '<h1> This is Adepitan Adetunji</h1><br><p>Thank you for checking out my page and project on DevOps. Stay tuned for my DevOps journey.</p> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
